eee54dcdeef541490d2e68e66003d3c244fb0986
干净 <54979375+du5@users.noreply.github.com>
2025-05-23 21:50:47 +0800

    更新 ethers 依赖至 6.14.1，添加 GetUserVote 路由以支持用户投票查询

--- src/endpoints/get.ts
+++ src/endpoints/get.ts
@@ -1,8 +1,8 @@
 import { Bool, OpenAPIRoute } from "chanfana";
 import { type AppContext } from "../types";
+import { ethers } from "ethers";
 
 export class GetVote extends OpenAPIRoute {
-
 	async handle(c: AppContext) {
 		const session = c.env.D1DB.withSession(`first-primary`);
 
@@ -26,6 +26,35 @@ export class GetVote extends OpenAPIRoute {
 			}
 
 			res.data[tx_hash][element.msg]++;
+		});
+
+		return res
+	}
+}
+
+export class GetUserVote extends OpenAPIRoute {
+	async handle(c: AppContext) {
+		const session = c.env.D1DB.withSession(`first-primary`);
+		// get user address from get param
+		const address = c.req.param("address");
+		const checksummed = ethers.getAddress(address);
+		if (checksummed === ethers.ZeroAddress) {
+			return { code: 400, msg: "Invalid address" };
+		}
+
+		const result = await session
+			.prepare(`SELECT * FROM vote_info WHERE vid = ? AND address = ?`)
+			.bind(c.req.param("voteID"))
+			.bind(checksummed)
+			.run();
+
+
+		if (result.error || !result.success) return { code: 500, msg: result.error };
+
+		let res = { "code": 200, "msg": null, "data": [] };
+		result.results.forEach(element => {
+			let tx_hash: any = element.tx_hash;
+			res.data.push(tx_hash);
 		});
 
 		return res
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { GetVote } from "./endpoints/get";
+import { GetVote, GetUserVote } from "./endpoints/get";
 import { PostVote } from "./endpoints/post";
 
 const app = new Hono<{ Bindings: Env }>();
@@ -8,6 +8,7 @@ const app = new Hono<{ Bindings: Env }>();
 const openapi = fromHono(app, {});
 
 openapi.get("/api/v2/vote/list/:voteID", GetVote);
+openapi.get("/api/v2/vote/user/:voteID", GetUserVote);
 openapi.post("/api/v2/vote/:voteID", PostVote);
 
 export default app;
