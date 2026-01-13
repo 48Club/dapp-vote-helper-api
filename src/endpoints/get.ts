f9f35e2e7f5622e4613d0829cea0c7726eda8692
干净 <54979375+du5@users.noreply.github.com>
2026-01-13 14:21:13 +0000

    添加 GetAllNodes 和 SM 路由，支持节点列表获取及节点请求处理

--- src/endpoints/get.ts
+++ src/endpoints/get.ts
@@ -1,4 +1,4 @@
-import { Bool, OpenAPIRoute } from "chanfana";
+import { OpenAPIRoute } from "chanfana";
 import { type AppContext } from "../types";
 import { ethers } from "ethers";
 
@@ -60,5 +60,64 @@ export class GetUserVote extends OpenAPIRoute {
 		});
 
 		return res
+	}
+}
+
+
+export class GetAllNodes extends OpenAPIRoute {
+	async handle(c: AppContext) {
+		const Authorization = c.req.header("Authorization");
+		if (Authorization !== c.env.API_TOKEN) return { code: 403, msg: "Forbidden" };
+
+		const session = c.env.D1SMDB.withSession(`first-primary`);
+
+		const result = await session.prepare(`SELECT * FROM nodes`).run();
+
+		if (result.error || !result.success) return { code: 500, msg: result.error };
+
+		let res = { "code": 200, "msg": null, "data": [] };
+
+		result.results.forEach(element => { res.data.push({ id: element.id, loc: element.loc, }) });
+
+		return res
+	}
+}
+
+export class SM extends OpenAPIRoute {
+	async handle(c: AppContext) {
+		const Authorization = c.req.header("Authorization");
+
+		if (Authorization !== c.env.API_TOKEN) return { code: 403, msg: "Forbidden" };
+
+		const nodeID = c.req.query("id");
+		if (!nodeID) return { "code": 400, "msg": "Node ID is required", "data": null };
+
+
+		const doAction = c.req.query("do_action");
+		var method = "GET";
+		if (["upgrade", "upgrade_geth"].includes(doAction)) method = "POST"; // 升级操作使用 POST 方法
+
+		var argsList = [];
+		const version = c.req.query("version");
+		if (version) argsList.push(`version=${version}`);
+
+		const result = await c.env.D1SMDB.prepare(`SELECT * FROM nodes WHERE id = ?`).bind(nodeID).first();
+		if (!result) return { "code": 400, "msg": "Node ID is required", "data": null } // 未找到对应节点
+		if (result.db_path) argsList.push(`db_path=${result.db_path}`); // 非空则添加参数
+		if (result.ipc_path) argsList.push(`ipc_path=${result.ipc_path}`);
+
+		const args = argsList.join("&");
+		var apiEndpoint = `http://${result.ip}.nip.io:30000/api/${doAction}`;
+		if (args) apiEndpoint += "?" + args;
+
+		const response = await fetch(apiEndpoint, {
+			method: method,
+			headers: {
+				'Authorization': Authorization,
+			},
+		});
+
+		if (response.status !== 200) return { "code": response.status, "msg": "Error from node", "data": null }
+		return await response.json();
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { GetVote, GetUserVote } from "./endpoints/get";
+import { GetVote, GetUserVote, GetAllNodes, SM } from "./endpoints/get";
 import { PostVote } from "./endpoints/post";
 
 const app = new Hono<{ Bindings: Env }>();
@@ -10,5 +10,8 @@ const openapi = fromHono(app, {});
 openapi.get("/api/v2/vote/list/:voteID", GetVote);
 openapi.get("/api/v2/vote/user/:voteID", GetUserVote);
 openapi.post("/api/v2/vote/:voteID", PostVote);
+
+openapi.get("/api/v2/sm/all", GetAllNodes); // 获取当前节点列表
+openapi.get("/api/v2/sm", SM); // 向节点后端发起请求
 
 export default app;
